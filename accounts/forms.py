41b875dc2ff17bcf2d5e197288a957b590d00331
Vladimir <vovchik161@gmail.com>
2015-05-05 02:27:00 +0300

    Accounts/forms.py Remove old Login form, create new register form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,22 @@
 __author__ = 'vladimir'
 
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-class Login(forms.Form):
-    username = forms.CharField(label='username', max_length=100, required=True)
-    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
