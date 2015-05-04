1c527a253fe2763738f559bce448a67655b3764d
Vladimir <vovchik161@gmail.com>
2015-05-05 02:29:19 +0300

    Car_market/urls.py Add url for register page

--- car_market/urls.py
+++ car_market/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from accounts.views import login
+
+from accounts.views import AccountRegistrationView
 
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login$', login)
+    # url(r'^accounts/login$', login)
+    url(r'register/$', AccountRegistrationView.as_view(), name='register')
 ]
