5a3a366999036d25e0dc66d73cd9c811d9a4378a
Vladimir <vovchik161@gmail.com>
2015-05-05 00:10:32 +0300

    Customers/models.py Add customer model

--- customers/models.py
+++ customers/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+
 from accounts.models import Account
+from utils.models import CarsModel
 
 
-class Customer(Account):
-    pass
+class Customer(models.Model):
+    account = models.ForeignKey(Account)
+    cars = models.ManyToManyField(CarsModel, blank=True, default=None)
+
+    def __unicode__(self):
+        return 'Customer: ' + self.account.user.get_full_name()
+
+    def save(self):
+        if self.account.type.type != 'C':
+            raise Exception("Account with type {} can't be a customer".format(self.account.type.type))
+        else:
+            super(Customer, self).save()
