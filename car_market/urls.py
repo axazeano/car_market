1e3b75d8de40c42ad845d1deda45973c029d31b5
Vladimir <vovchik161@gmail.com>
2015-05-18 09:15:35 +0300

    Customers/views.py Some

--- car_market/urls.py
+++ car_market/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from accounts.views import AccountRegistrationView
+from accounts.views import AccountRegistrationView, LoginView, logout, dashboard_view
 
 
 urlpatterns = [
@@ -11,5 +11,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^accounts/login$', login)
-    url(r'register/$', AccountRegistrationView.as_view(), name='register')
+    url(r'^accounts/register/$', AccountRegistrationView.as_view(), name='register'),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/dashboard/$', dashboard_view, name='dashboard'),
+    url(r'^accounts/logout/$', logout, name='logout')
 ]
