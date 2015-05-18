7f444a123bca54b5620afe90296dbd9df31a0271
Vladimir <vovchik161@gmail.com>
2015-05-19 01:39:55 +0300

    Utils/admin.py Customize Message and Order pages in Admin

--- utils/admin.py
+++ utils/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import CarsMark, CarsModel, Country, Warehouse, WarehouseItem, Part, PartType
+
+from .models import CarsMark, CarsModel, Country, Warehouse, WarehouseItem, Part, PartType, Message, Order
+
 
 admin.site.register(Country)
 admin.site.register(CarsModel)
@@ -8,5 +10,18 @@ admin.site.register(Warehouse)
 admin.site.register(WarehouseItem)
 admin.site.register(Part)
 admin.site.register(PartType)
+# admin.site.register(Message)
+# admin.site.register(Order)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'to', 'body', 'time', 'is_unread')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'to', 'part', 'count', 'cost', 'additional_cost')
+
 
 
