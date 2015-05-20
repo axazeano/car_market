68c822417c9a2685ccc596a4dcec5fc218dc9ccc
Vladimir <vovchik161@gmail.com>
2015-05-21 01:25:25 +0300

    Fabrics/model.py Add country field

--- fabrics/models.py
+++ fabrics/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+
 from accounts.models import Account
+from utils.models import Country
 
 
 class Fabric(models.Model):
     name = models.CharField(max_length=120, null=False, blank=False, unique=True)
-    owner = models.ForeignKey('Account')
+    owner = models.OneToOneField(Account)
     balance = models.FloatField(null=False, blank=False, default=500000)
+    country = models.ForeignKey(Country, null=True, blank=True)
+
 
 
