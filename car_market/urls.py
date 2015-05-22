69932a6b6fce1e30bb17553856d98b41daea7d6d
vladimir Kubantsev <vovchik161@gmail.com>
2015-05-22 17:04:41 +0300

    Add separated main urls.py

--- car_market/urls.py
+++ car_market/urls.py
@@ -1,25 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-
-from accounts.views import AccountRegistrationView, LoginView, logout, dashboard_view
-from fabrics.views import create_fabric
 from utils.views import IndexView
 
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'car_market.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    # url(r'^accounts/login$', login)
-    url(r'^accounts/register/$', AccountRegistrationView.as_view(), name='register'),
-    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
-    url(r'^accounts/dashboard/$', dashboard_view, name='dashboard'),
-    url(r'^accounts/logout/$', logout, name='logout'),
-    url(r'^$', IndexView.as_view(), name='index'),
-
-    url(r'^fabrics/create/$', create_fabric, name='create_fabric'),
-
-    url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^admin/', include(admin.site.urls)),                          # import main admin urls
+    url(r'^$', IndexView.as_view(), name='index'),                      # index view
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')), # import accounts urls
+    url(r'^fabrics/', include('fabrics.urls', namespace='fabrics')),    # imports fabrics urls
+    url(r'^admin_tools/', include('admin_tools.urls')),                 # import admin-tools url
 ]
