9bc443ad2d9cc0e4083aa63aca739c534117bb11
vladimir Kubantsev <vovchik161@gmail.com>
2015-05-22 22:51:51 +0300

    Accounts/models.py
    Remove account model
    Create abstract model AbstractAccount

--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,9 @@
+from utils.models import Country
+
 __author__ = 'vladimir'
 
 from django.db import models
 from django.contrib.auth.models import User
-
-"""
- All models must have __unicode__() method for properly representation!
-"""
-
-
-class AccountsType(models.Model):
-    type = models.CharField(max_length=1, null=False, blank=False, unique=True, primary_key=True)
-    description = models.CharField(max_length=100, null=True, blank=True)
-
-
-    def __unicode__(self):
-        return self.type + ' (' + self.description + ')'
-
-
-class Account(models.Model):
-    user = models.ForeignKey(User)
-    type = models.ForeignKey('AccountsType', to_field='type')
-    balance = models.FloatField(null=False, blank=False)
-    is_fully_created = models.BooleanField(null=False, blank=False, default=False)
-
-    def __unicode__(self):
-        return self.user.get_full_name() + ' ' + self.type.__unicode__() + ' #' + str(self.pk)
-
-    @staticmethod
-    def get_all_accounts_of_user(user):
-        return Account.objects.filter(user=user)
 
 
 class Worker(models.Model):
@@ -37,12 +12,22 @@ class Worker(models.Model):
     salary = models.FloatField(default=0, null=False, blank=False)
     is_active = models.BooleanField(default=True, null=False, blank=False)
     department = models.CharField(max_length=120, default='')
-    owner = models.ForeignKey(Account)
+    owner = models.ForeignKey(User)
 
     @staticmethod
-    def create_many_workers(account, count, salary=0, is_active=True):
+    def create_many_workers(user, count, salary=0, is_active=True):
         for x in xrange(count):
-            worker = Worker(owner=account,
+            worker = Worker(owner=user,
                             salary=salary,
                             is_active=is_active)
             worker.save()
+
+
+class AbstractAccount(models.Model):
+    owner = models.OneToOneField(User, default='')
+    name = models.CharField(max_length=200, unique=True, default='')
+    balance = models.FloatField(default=0.0, null=False, blank=False)
+    country = models.ForeignKey(Country, default='')
+
+    class Meta:
+        abstract = True
