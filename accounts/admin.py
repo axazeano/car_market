f903ac5cb33ef8ef79f4804a572fc336e0916f33
Vladimir <vovchik161@gmail.com>
2015-05-22 12:16:23 +0300

    Accounts/admin.py
    Register worker model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Account, AccountsType
+
+from .models import Account, AccountsType, Worker
+
 
 # Register your models here.
 
 admin.site.register(Account)
 admin.site.register(AccountsType)
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'surname', 'department', 'salary', 'is_active')
