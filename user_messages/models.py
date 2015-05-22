d778ead0fd9766333828b49bc994ab3f9cf06521
vladimir Kubantsev <vovchik161@gmail.com>
2015-05-22 23:01:48 +0300

    User_messages/models.py
    Remove an Account model form a Message model. Using User instead of Account

--- user_messages/models.py
+++ user_messages/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.utils.timezone import now
-from accounts.models import Account
-
+from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.ForeignKey(Account, related_name='sender')
-    to = models.ForeignKey(Account, related_name='to')
+    sender = models.ForeignKey(User, related_name='sender')
+    to = models.ForeignKey(User, related_name='to')
     title = models.CharField(max_length=120, null=True, blank=True, default='Empty title')
     body = models.TextField(null=False, blank=False, default='')
     send_time = models.DateTimeField(null=False, blank=False, default=now)
